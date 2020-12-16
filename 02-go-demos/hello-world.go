10126d8067b0da50344801537b5753cb1a93c5f0
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-16 14:19:10 +0530

    variable declarations options

--- 02-go-demos/hello-world.go
+++ 02-go-demos/hello-world.go
@@ -4,6 +4,47 @@ import "fmt" //importing dependant packages
 
 //package level variables
 
+//declaring 1 variable
+var id int
+
+//declaring multiple variables
+/*
+var (
+	name string
+	age int
+	location string
+)
+*/
+
+/*
+var (
+	name, location string
+	age int
+)
+*/
+
+//variables with initilizers
+/*
+var (
+	name string = "Magesh Kuppan"
+	age int = 35
+	location string = "Bengaluru"
+)
+*/
+
+//data types can be omitted when initialized
+/*
+var (
+	name = "Magesh Kuppan"
+	age = 35
+	location = "Bengaluru"
+)
+*/
+
+var (
+	name, age, location = "Magesh Kuppan", 35, "Bengaluru"
+)
+
 func init() { // function executed when the package is initialized
 
 }
