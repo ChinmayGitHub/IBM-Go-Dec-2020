ccd3415dde5724354b6c29be4f83b79cb2765be0
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-16 14:17:22 +0530

    variable declaration options

---
+++ 02-go-demos/hello-world.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt" //importing dependant packages
+
+//package level variables
+
+func init() { // function executed when the package is initialized
+
+}
+
+func main() {
+	fmt.Println("Welcome to Golang!")
+}
+
+//interfaces
+
+//struct
+
+//functions
