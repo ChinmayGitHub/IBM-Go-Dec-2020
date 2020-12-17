2f57b09a5b9d9160b25556d00662b00d923d7d66
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-17 12:23:07 +0530

    methods demo

--- 02-go-demos/structs-demo.go
+++ 02-go-demos/structs-demo.go
@@ -9,7 +9,36 @@ type Person struct {
 	City       string
 }
 
-func main() {
-	var p = Person{Id: 100, Name: "Magesh", IsEmployed: true, City: "Bangalore"}
+//as functions
+/*
+func  display(p Person){
 	fmt.Println(p)
+	return
+}
+
+func  toggleEmploymentStatus (p *Person) {
+	p.IsEmployed = !p.IsEmployed
+}
+*/
+
+func (p Person) display() {
+	fmt.Println(p)
+	return
+}
+
+func (p *Person) toggleEmploymentStatus() {
+	p.IsEmployed = !p.IsEmployed
+}
+
+func main() {
+	/* var p = Person{Id: 100, Name: "Magesh", IsEmployed: true, City: "Bangalore"} */
+	p := &Person{100, "Magesh", true, "Bangalore"}
+	/*
+		display(p)
+		toggleEmploymentStatus(&p)
+		display(p)
+	*/
+	p.display()
+	p.toggleEmploymentStatus()
+	p.display()
 }
