44f15268fd0c1e2c7237c8d572e4b8d1c158f250
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-17 16:28:51 +0530

    using channels

--- 03-concurrency-demos/demo-02.go
+++ 03-concurrency-demos/demo-02.go
@@ -5,14 +5,20 @@ import (
 	"time"
 )
 
-func say(s string) {
+func say(s string, c chan int, id int) {
 	for i := 0; i < 5; i++ {
 		time.Sleep(time.Millisecond * 100)
 		fmt.Println(s)
 	}
+	c <- id
 }
 
 func main() {
-	go say("Hello")
-	go say("World")
+	c1 := make(chan int)
+	c2 := make(chan int)
+	go say("Hello", c1, 1)
+	go say("World", c2, 2)
+	fmt.Println(<-c1)
+	fmt.Println(<-c2)
+	fmt.Println("End of application")
 }
