38e6fd50ccdba6725cfd5a480dac9793a68f0d3c
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-18 09:58:57 +0530

    updates

--- 03-concurrency-demos/demo-03.go
+++ 03-concurrency-demos/demo-03.go
@@ -4,32 +4,39 @@ import (
 	"fmt"
 )
 
-func sum(list []int, c chan int) {
+func sum(list []int, goId int, c chan int) {
 	sum := 0
 	for _, v := range list {
 		sum += v
 	}
+	fmt.Printf("before - goId %d returning result %d \n", goId, sum)
 	c <- sum
+	fmt.Printf("after - goId %d returning result \n", goId)
 }
 
 func main() {
 	nos := []int{3, 6, 1, 4, 2, 5}
+
+	channel := make(chan int)
+	//secondChan := make(chan int)
+	first := nos[:len(nos)/2]
+	second := nos[len(nos)/2:]
+	go sum(first, 1, channel)
+	go sum(second, 2, channel)
+	fmt.Println("receiving firstTotal")
+	firstTotal := <-channel
+	fmt.Printf("received firstTotal - %d\n", firstTotal)
+	fmt.Println("receiving secondTotal")
+	secondTotal := <-channel
+	fmt.Printf("received secondTotal - %d\n", secondTotal)
+	total := firstTotal + secondTotal
+	fmt.Println(total)
+
 	/*
-		firstChan := make(chan int)
-		secondChan := make(chan int)
-		first := nos[:len(nos)/2]
-		second := nos[len(nos)/2:]
-		go sum(first, firstChan)
-		go sum(second, secondChan)
-		firstTotal := <-firstChan
-		secondTotal := <-secondChan
-		total := firstTotal + secondTotal
+		c := make(chan int)
+		go sum(nos[:len(nos)/2], c)
+		go sum(nos[len(nos)/2:], c)
+		total := <-c + <-c
 		fmt.Println(total)
 	*/
-
-	c := make(chan int)
-	go sum(nos[:len(nos)/2], c)
-	go sum(nos[len(nos)/2:], c)
-	total := <-c
-	fmt.Println(total)
 }
