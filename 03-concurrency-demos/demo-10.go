53ec3a4c1f89ab2328651074b06e33cf7434fe67
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-18 12:07:15 +0530

    timeout assignment solution

--- 03-concurrency-demos/demo-10.go
+++ 03-concurrency-demos/demo-10.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
-	resp, err := http.Get("http://info.cern.ch/hypertext/WWW/TheProject.html")
-	if err != nil {
-		fmt.Println(err)
-		return
+	response := make(chan *http.Response, 1)
+	//errors := make(chan *error)
+	go func() {
+		resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+		if err != nil {
+			fmt.Println("Something went wrong")
+		}
+		response <- resp
+	}()
+	for {
+		select {
+		case res := <-response:
+			io.Copy(os.Stdout, res.Body)
+			return
+		case <-time.After(100 * time.Millisecond):
+			fmt.Println("timeout!")
+			return
+		}
 	}
-	fmt.Println(resp)
+
 }
