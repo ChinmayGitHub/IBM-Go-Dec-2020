da9422ba69af4c49558350568a25483e705db0d6
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-18 12:17:17 +0530

    wait groups - demo

---
+++ 03-concurrency-demos/demo-11.go
@@ -0,0 +1,39 @@
+//WaitGroup
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func goRoutine1(wg *sync.WaitGroup) {
+	time.Sleep(5000 * time.Millisecond)
+	wg.Done()
+	fmt.Println("go routine 1 completed")
+}
+
+func goRoutine2(wg *sync.WaitGroup) {
+	time.Sleep(2000 * time.Millisecond)
+	wg.Done()
+	fmt.Println("go routine 2 completed")
+}
+
+func goRoutine3(wg *sync.WaitGroup) {
+	time.Sleep(1000 * time.Millisecond)
+	wg.Done()
+	fmt.Println("go routine 3 completed")
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go goRoutine1(wg)
+	wg.Add(1)
+	go goRoutine2(wg)
+	wg.Add(1)
+	go goRoutine3(wg)
+	wg.Wait()
+	fmt.Println("main completed")
+}
