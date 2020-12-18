f2e175bf97b42638881b7aad6da7a7f2407f3688
Magesh Kuppan <tkmagesh77@gmail.com>
2020-12-18 15:58:50 +0530

    basic web server

---
+++ 05-network/http/ping.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloServer(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello %s ", req.URL.Path[1:])
+}
+func main() {
+	http.HandleFunc("/", helloServer)
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
